729a5e1867bf05a4728fe8a8d3ce724b05d188d4
damianrein <damianrein464@gmail.com>
2024-05-28 20:39:47 -0300

    -

---
+++ src/main/java/com/ApiBatchChunk/ApiBatchChunkApplication.java
@@ -0,0 +1,13 @@
+package com.ApiBatchChunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBatchChunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBatchChunkApplication.class, args);
+	}
+
+}
